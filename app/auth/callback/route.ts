bcd75eb5a4c8da68526cc182d73016e064077ba6
amitdhiman5086 <amitdhiman5086@gmail.com>
2026-01-19 11:44:11 +0530

    feat: added auth callback

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,28 @@
+import { getSupabaseClient } from "@/app/supabase-client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const host = request.headers.get("host");
+  const origin = process.env.NODE_ENV === "development" ? `http://${host}` : `https://${host}`;
+
+  const code = searchParams.get("code");
+  const type = searchParams.get("type") ?? "";
+  const next = "/editor";
+  console.log("searchParams", request.url, code, type, next);
+
+  if (code) {
+    const supabase = await getSupabaseClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      if (type === "reset") {
+        return NextResponse.redirect(`${origin}/reset-password`);
+      }
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  // Return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/editor`);
+}
