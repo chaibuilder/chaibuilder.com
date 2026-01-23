f0688b7d9adec8522e88e316c352adbacfc600c9
Suraj Air <surajair@gmail.com>
2026-01-23 15:29:37 +0530

    chore: remove empty theme object from editor Tailwind config

--- app/(builder)/editor/tailwind.config.ts
+++ app/(builder)/editor/tailwind.config.ts
@@ -2,5 +2,4 @@ import { getChaiBuilderTailwindConfig } from "@chaibuilder/next/utils";
 
 export default getChaiBuilderTailwindConfig({
   content: ["./node_modules/@chaibuilder/next/dist/**/*.{js,cjs}"],
-  theme: {},
 });
