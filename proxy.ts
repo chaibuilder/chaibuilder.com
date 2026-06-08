eaa9d2d10ef4ba3ed725c7fe29366932070f6125
amitdhiman5086 <amitdhiman5086@gmail.com>
2026-06-08 12:19:06 +0530

    feat: add edge proxy middleware to handle path revalidation redirects

---
+++ proxy.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export default function proxy(request: NextRequest) {
+    if (request.nextUrl.searchParams.has("r")) {
+        const url = new URL(request.nextUrl.href);
+        url.searchParams.delete("r");
+        return NextResponse.redirect(
+            `${url.origin}/api/revalidate?redirect=true&paths=${url.pathname}`,
+        );
+    }
+    return NextResponse.next();
+}
